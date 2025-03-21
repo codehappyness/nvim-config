aaf80dad684eb4334e913b394e67e291f4b4e390
Nam Trung Truong <codehappyness@gmail.com>
2025-03-21 19:43:16 +0700

    Update Learning

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,8 +130,11 @@ return {
         rust_analyzer = {
           settings = {
             ["rust-analyzer"] = {
-              cargo = { allFeatures = true },
-              procMacro = { enable = true },
+              --cargo = { allFeatures = true },
+              --procMacro = { enable = true },
+              diagnostics = {
+                enable = false,
+              },
             },
           },
         },
@@ -215,9 +218,9 @@ return {
       if opts.inlay_hints.enabled then
         LazyVim.lsp.on_supports_method("textDocument/inlayHint", function(client, buffer)
           if
-              vim.api.nvim_buf_is_valid(buffer)
-              and vim.bo[buffer].buftype == ""
-              and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buffer].filetype)
+            vim.api.nvim_buf_is_valid(buffer)
+            and vim.bo[buffer].buftype == ""
+            and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buffer].filetype)
           then
             vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
           end
@@ -238,14 +241,14 @@ return {
 
     if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
       opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
-          or function(diagnostic)
-            local icons = LazyVim.config.icons.diagnostics
-            for d, icon in pairs(icons) do
-              if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
-                return icon
-              end
+        or function(diagnostic)
+          local icons = LazyVim.config.icons.diagnostics
+          for d, icon in pairs(icons) do
+            if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+              return icon
             end
           end
+        end
     end
 
     vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
