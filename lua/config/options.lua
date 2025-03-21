310732868806f73604fbf48ff4575bf512daef04
Nam Trung Truong <codehappyness@gmail.com>
2025-03-21 08:24:00 +0700

    Update Config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,3 +18,6 @@ vim.opt.foldmarker = "#region,#endregion"
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = true
+-- In case you don't want to use `:LazyExtras`,
+-- then you need to set the option below.
+vim.g.lazyvim_picker = "snacks"
---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/todo-comments.nvim",
+  optional = true,
+  -- stylua: ignore
+  keys = {
+    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+  },
+}
