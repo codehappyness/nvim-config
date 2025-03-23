119d4cce479d667c71e999662348d6cfef7df422
Nam Trung Truong <codehappyness@gmail.com>
2025-03-23 20:01:37 +0700

    Update config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,7 +42,7 @@ return {
       -- Be aware that you also will need to properly configure your LSP server to
       -- provide the code lenses.
       codelens = {
-        enabled = false,
+        enabled = true,
       },
       -- add any global capabilities here
       capabilities = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,14 +16,52 @@ return {
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   keys = {
     { "<c-space>", desc = "Increment Selection" },
-    { "<bs>",      desc = "Decrement Selection", mode = "x" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
   },
   opts_extend = { "ensure_installed" },
   ---@type TSConfig
   ---@diagnostic disable-next-line: missing-fields
+  dependencies = {
+    -- Add nvim-ts-autotag
+    "windwp/nvim-ts-autotag",
+    -- "nvim-treesitter/nvim-treesitter-context",
+    "HiPhish/nvim-ts-rainbow2",
+    "nvim-treesitter/playground",
+    "windwp/nvim-autopairs",
+    "posva/vim-vue",
+  },
+  -- https://github.com/nvim-treesitter/playground#query-linter
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = { "BufWrite", "CursorHold" },
+  },
+
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = true, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = "o",
+      toggle_hl_groups = "i",
+      toggle_injected_languages = "t",
+      toggle_anonymous_nodes = "a",
+      toggle_language_display = "I",
+      focus_language = "f",
+      unfocus_language = "F",
+      update = "R",
+      goto_node = "<cr>",
+      show_help = "?",
+    },
+  },
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
+  autotag = {
+    -- Setup autotag using treesitter config.
+    enable = true,
+  },
     ensure_installed = {
       "bash",
       "php",
