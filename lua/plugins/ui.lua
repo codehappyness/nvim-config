5d49083e2e86a94761cc2ff5b7f1e5459f1e4fd5
Nam Trung Truong <codehappyness@gmail.com>
2025-03-24 13:42:14 +0700

    Update Config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -62,10 +62,10 @@ return {
     },
     opts = {
       options = {
-        --mode = "tabs",
+        mode = "tabs",
         --separator_style = "slant",
-        --show_buffer_close_icons = false,
-        --show_close_icon = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
       },
     },
   },
