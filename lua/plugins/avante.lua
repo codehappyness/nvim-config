f495306f3e071660f080ec22b48060468f5eae8e
Nam Trung Truong <codehappyness@gmail.com>
2025-08-26 07:19:55 +0700

    Migration for avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,19 +3,45 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  ---- other configuration items ...
   opts = {
-    -- add any opts here
-    -- for example
-    provider = "openai",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o",             -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    --- other configuration items ...
+    providers = {
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        extra_request_body = {
+          temperature = 0,
+          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+          reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+      },
+      -- ollama = {
+      --   endpoint = "http://127.0.0.1:11434",
+      --   timeout = 30000, -- Timeout in milliseconds
+      --   extra_request_body = {
+      --     options = {
+      --       temperature = 0.75,
+      --       num_ctx = 20480,
+      --       keep_alive = "5m",
+      --     },
+      --   },
+      -- },
+      -- groq = {
+      --   __inherited_from = 'openai',
+      --   api_key_name = 'GROQ_API_KEY',
+      --   endpoint = 'https://api.groq.com/openai/v1/',
+      --   model = 'llama-3.3-70b-versatile',
+      --   disable_tools = true,
+      --   extra_request_body = {
+      --     temperature = 1,
+      --     max_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+      --   },
+      -- },
     },
   },
+  --- other configuration items-
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
